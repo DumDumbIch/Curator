9eb47f46dcfdcd147696e7640df43402d71367d9
DumDumbIch <dumdumbich@gmail.com>
2021-04-08 20:50:39 +0300

    Refactoring App

--- app/src/main/java/com/dumdumbich/curator/ui/App.kt
+++ app/src/main/java/com/dumdumbich/curator/ui/App.kt
@@ -1,26 +1,44 @@
 package com.dumdumbich.curator.ui
 
 import android.app.Application
-import com.github.terrakok.cicerone.Cicerone
-import com.github.terrakok.cicerone.Router
+import android.util.Log
+import com.dumdumbich.curator.di.scope.main.AppComponent
+import com.dumdumbich.curator.di.scope.main.DaggerAppComponent
+import com.dumdumbich.curator.di.scope.main.module.AppModule
+import com.dumdumbich.curator.di.scope.mentees.IMenteesScopeContainer
+import com.dumdumbich.curator.di.scope.mentees.MenteesSubcomponent
 
-class App : Application() {
+
+const val LOG_D_TAG = "DUMDUMBICH_CURATOR"
+
+class App : Application(), IMenteesScopeContainer {
 
     companion object {
         lateinit var instance: App
     }
 
-    private val cicerone: Cicerone<Router> by lazy {
-        Cicerone.create()
-    }
+    lateinit var appComponent: AppComponent
 
-    val navigatorHolder get() = cicerone.getNavigatorHolder()
+    var menteesSubcomponent: MenteesSubcomponent? = null
+        private set
 
-    val router get() = cicerone.router
 
     override fun onCreate() {
+        Log.d(LOG_D_TAG, "App(): onCreate()")
         super.onCreate()
         instance = this
+
+        appComponent = DaggerAppComponent.builder()
+            .appModule(AppModule(this))
+            .build()
+    }
+
+    fun initMenteesSubcomponent() = appComponent.menteesSubcomponent().also {
+        menteesSubcomponent = it
+    }
+
+    override fun releaseMenteesScope() {
+        menteesSubcomponent = null
     }
 
 }
