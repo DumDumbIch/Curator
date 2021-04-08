bce45f5d91e042d9b656815e2abd439711ac769e
DumDumbIch <dumdumbich@gmail.com>
2021-04-08 20:55:10 +0300

    Refactoring navigation

--- app/src/main/java/com/dumdumbich/curator/ui/navigator/IScreens.kt
+++ app/src/main/java/com/dumdumbich/curator/ui/navigator/IScreens.kt
@@ -1,7 +1,9 @@
 package com.dumdumbich.curator.ui.navigator
 
+import com.dumdumbich.curator.domain.entity.Mentee
 import com.github.terrakok.cicerone.Screen
 
 interface IScreens {
-    fun mentees():Screen
+    fun mentees(): Screen
+    fun mentee(mentee: Mentee): Screen
 }
